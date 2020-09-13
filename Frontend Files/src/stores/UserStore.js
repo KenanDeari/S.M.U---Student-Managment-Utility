79f467640143c56dfe7440e578455880abac95a7
SSVF2 <kulpreet.s18@gmail.com>
2020-09-12 22:45:48 -0500

    adding frontend file public and src

---
+++ Frontend Files/src/stores/UserStore.js
@@ -0,0 +1,13 @@
+import { extendObservable } from 'mobx';
+
+class UserStore {
+    constructor () {
+        extendObservable(this, {
+            loading: true,
+            isLoggedIn: false,
+            username: ''
+        })
+    }
+}
+
+export default new UserStore ();
